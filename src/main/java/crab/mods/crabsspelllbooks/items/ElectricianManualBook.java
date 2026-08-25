509703521158980ddca00ba7c87839be13357767
THECRABSPILOT <vbcjrmoore.13@gmail.com>
2026-08-25 00:21:15 -0700

    got it working

--- src/main/java/crab/mods/crabsspelllbooks/items/ElectricianManualBook.java
+++ src/main/java/crab/mods/crabsspelllbooks/items/ElectricianManualBook.java
@@ -1,14 +1,21 @@
 package crab.mods.crabsspelllbooks.items;
 
-import io.redspace.ironsspellbooks.item.SpellBook;
+import io.redspace.ironsspellbooks.api.registry.AttributeRegistry;
+import io.redspace.ironsspellbooks.api.spells.SpellRarity;
+import io.redspace.ironsspellbooks.item.spell_books.SimpleAttributeSpellBook;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.Rarity;
 
-public class ElectricianManualBook extends SpellBook {
+public class ElectricianManualBook extends SimpleAttributeSpellBook {
 
     public ElectricianManualBook() {
-        super(8, new Item.Properties()
-                .stacksTo(1)
-                .rarity(Rarity.EPIC));
+        // slots, rarity, attribute, +10% power, +200 mana
+        super(
+                12,
+                SpellRarity.EPIC,
+                AttributeRegistry.LIGHTNING_SPELL_POWER.get(),
+                0.10,
+                200
+        );
     }
 }
